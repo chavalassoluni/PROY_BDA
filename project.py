67ca8cab4cd0632cbdd7443c07db306f8eeecb79
Nidian Lasso <nidianlassoch@hotmail.com>
2023-11-29 22:43:08 -0500

    solucion busquedas de empleados por sucursal y creacion de templetes para el flujo de compra

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_mysqldb import MySQL
-from query import obtenerEmpleados, validarLogin, validacionUsuarioSucursal
+from query import obtenerEmpleados, validarLogin, validacionUsuarioSucursal, obtener_info_cliente, listadoLibrosBarranquilla, listadoLibrosMedellin, listadoLibrosSantaMarta, listadoLibrosCartagena, listadoLibrosRiohacha, listadoLibrosBogota, listadoLibrosPasto, listadoLibrosCali, listadoEmpMedellin
 from flask import Flask, jsonify, render_template, request, redirect, url_for, flash, session
 app = Flask(__name__, static_folder='static', template_folder='template')
 
@@ -29,52 +29,85 @@ def index():
 def cliente():
      identificacionCliente = request.cookies.get('identificacion')
      clienteSucursal=validacionUsuarioSucursal(identificacionCliente)
-     return render_template('./cliente.html', cliente=clienteSucursal)
+     return render_template('./cliente.html', clientes=clienteSucursal)
+
+@app.route('/perfilCliente')
+def clientePerfil():
+     clientePerfil=obtener_info_cliente()
+     return render_template('./cliente.html', perfilCliente=clientePerfil)
+
+
+
+@app.route('/Barranquilla')
+def comprarLibroCliente():
+     librosBarranquilla = listadoLibrosBarranquilla()
+     return render_template('./barranquilla.html', librosB=librosBarranquilla)
+
+@app.route('/medellin')
+def comprarLibroClienteMedellin():
+     librosMedellin = listadoLibrosMedellin()
+     return render_template('./medellin.html', librosM=librosMedellin)
+
+@app.route('/santaMarta')
+def comprarLibroClienteSantaMartha():
+     librosSnMarta = listadoLibrosSantaMarta()
+     return render_template('./santaMarta.html', librosS=librosSnMarta)
+
+@app.route('/cartagena')
+def comprarLibroClienteCartagena():
+     librosCartagena = listadoLibrosCartagena()
+     return render_template('./cartagena.html', librosC=librosCartagena)
+
+@app.route('/riohacha')
+def comprarLibroClienteRiohacha():
+     librosRiohacha = listadoLibrosRiohacha()
+     return render_template('./riohacha.html', librosR=librosRiohacha)
+
+@app.route('/bogota')
+def comprarLibroClienteBogota():
+     librosBogota = listadoLibrosBogota()
+     return render_template('./bogota.html', librosBg=librosBogota)
+
+@app.route('/pasto')
+def comprarLibroClientePasto():
+     librosPasto = listadoLibrosPasto()
+     return render_template('./pasto.html', librosP=librosPasto)
+
+@app.route('/cali')
+def comprarLibroClienteCali():
+     librosCali = listadoLibrosCali()
+     return render_template('./cali.html', librosCali=librosCali)
+
+
+#OBTENER INFO DE EMPLEADOS POR SUCURSAL
+@app.route('/empMedellin')
+def infoEmpMedellin():
+     infoEmpMedellin = listadoEmpMedellin()
+     return render_template('./empMedellin.html', infoEmple=infoEmpMedellin)
+
+@app.route('/comprarLibroC')
+def comprarLibroC():
+     return render_template('./comprarLibroC.html')
 
 @app.route('/consolidado')
 def conoslidado():
      return render_template('./consolidado.html')
 
-
-@app.route('/barranquilla')
-def barranquilla():
-     return render_template('./barranquilla.html')
-
-@app.route('/bogota')
-def bogota():
-     return render_template('./bogota.html')
-
-@app.route('/cali')
-def cali():
-     return render_template('./cali.html')
-
-@app.route('/cartagena')
-def cartagena():
-     return render_template('./cartagena.html')
-
-@app.route('/medellin')
-def medellin():
-     return render_template('./medellin.html')
-
-@app.route('/pasto')
-def pasto():
-     return render_template('./pasto.html')
-
-@app.route('/riohacha')
-def riohacha():
-     return render_template('./riohacha.html')
-
-@app.route('/santaMarta')
-def santaMarta():
-     return render_template('./santaMarta.html')
-
+@app.route('/reportes')
+def reportes():
+     return render_template('./reportes.html')
 
 @app.route('/gerente')
 def gerente():
      return render_template('./gerente.html')
 
+@app.route('/agregarEmpleado')
+def agregarEmpleado():
+     return render_template('./agregarEmpleado.html')
 
-
+@app.route('/sucursalesCompra')
+def sucursalesCompra():
+     return render_template('./sucursalesCompra.html')
 
 if __name__ =='__main__':
     app.run(port =3000, debug =True)
--- query.py
+++ query.py
@@ -16,7 +16,7 @@ def validarLogin(identificacion, contrasena):
         # Verificar las credenciales en la base de datos
     if check_credentials(identificacion, contrasena):
         #GUARDAR COKKIE
-        resp = make_response(redirect(url_for('index')))
+        resp = make_response(redirect(url_for('gerente')))
         resp.set_cookie('identificacion', identificacion)
         return resp
     
@@ -34,18 +34,75 @@ def validarLogin(identificacion, contrasena):
 def check_credentials(identificacion, contrasena):
     # Ejemplo de consulta para verificar las credenciales
     cursor.execute("SELECT idEmpleado, NombreEmpleado, DocumentoEmpleado FROM leerencanta.empleado WHERE DocumentoEmpleado = %s AND Contrasena =%s", (identificacion, contrasena))
-    info_empleado = cursor.fetchone()
+    info_empleado = cursor.fetchall()
     return info_empleado is not None
 
 
 def validacionUsuarioSucursal(identificacion):
     cursor.execute("SELECT * FROM empleado e INNER JOIN sucursal s ON e.NombreSucursal = s.NombreSucursal WHERE e.DocumentoEmpleado = %s", (identificacion))
-    info_empleadoSucursalLogueo = cursor.fetchone()
+    info_empleadoSucursalLogueo = cursor.fetchall()
     return info_empleadoSucursalLogueo
 
 
 def check_credentials_cliente(identificacion, contrasena):
     # Ejemplo de consulta para verificar las credenciales
     cursor.execute("SELECT idCliente, NombreCliente, DocumentoCliente FROM leerencanta.cliente WHERE DocumentoCliente = %s AND Contrasena =%s", (identificacion, contrasena))
-    info_cliente = cursor.fetchone()
+    info_cliente = cursor.fetchall()
     return info_cliente is not None
+
+
+def obtener_info_cliente():
+    cursor.execute("SELECT DocumentoCliente, NombreCliente, CiudadResidencia, Contrasena, LIbrosComprados FROM leerencanta.cliente")
+    info_miembro = cursor.fetchall()
+    return info_miembro
+
+#CONSULTA LIBROS DISPONIBLES EN BARRANQUILLA
+def listadoLibrosBarranquilla():
+    cursor.execute("SELECT libro.Titulo, sucursal.NombreSucursal FROM inventario JOIN libro ON inventario.idLibro = libro.idLibro JOIN sucursal ON inventario.NombreSucursal = sucursal.NombreSucursal WHERE sucursal.NombreSucursal='Arena de ideas'")
+    listadoBarranquilla = cursor.fetchall()
+    return listadoBarranquilla
+
+#CONSULTA LIBROS DISPONIBLES EN MEDELLIN
+def listadoLibrosMedellin():
+    cursor.execute("SELECT libro.Titulo, sucursal.NombreSucursal FROM inventario JOIN libro ON inventario.idLibro = libro.idLibro JOIN sucursal ON inventario.NombreSucursal = sucursal.NombreSucursal WHERE sucursal.NombreSucursal='Librero del valle'")
+    listadoLibrosMedellin = cursor.fetchall()
+    return listadoLibrosMedellin
+
+#CONSULTA LIBROS DISPONIBLES EN SANTA MARTA
+def listadoLibrosSantaMarta():
+    cursor.execute("SELECT libro.Titulo, sucursal.NombreSucursal FROM inventario JOIN libro ON inventario.idLibro = libro.idLibro JOIN sucursal ON inventario.NombreSucursal = sucursal.NombreSucursal WHERE sucursal.NombreSucursal='Mar de libros'")
+    listadoLibrosSantaMarta = cursor.fetchall()
+    return listadoLibrosSantaMarta
+
+#CONSULTA LIBROS DISPONIBLES EN CARTAGENA
+def listadoLibrosCartagena():
+    cursor.execute("SELECT libro.Titulo, sucursal.NombreSucursal FROM inventario JOIN libro ON inventario.idLibro = libro.idLibro JOIN sucursal ON inventario.NombreSucursal = sucursal.NombreSucursal WHERE sucursal.NombreSucursal='Murallas del saber'")
+    listadoLibrosCartagena = cursor.fetchall()
+    return listadoLibrosCartagena
+
+def listadoLibrosRiohacha():
+    cursor.execute("SELECT libro.Titulo, sucursal.NombreSucursal FROM inventario JOIN libro ON inventario.idLibro = libro.idLibro JOIN sucursal ON inventario.NombreSucursal = sucursal.NombreSucursal WHERE sucursal.NombreSucursal='Oasis de lectura'")
+    listadoLibrosRiohacha = cursor.fetchall()
+    return listadoLibrosRiohacha
+
+def listadoLibrosBogota():
+    cursor.execute("SELECT libro.Titulo, sucursal.NombreSucursal FROM inventario JOIN libro ON inventario.idLibro = libro.idLibro JOIN sucursal ON inventario.NombreSucursal = sucursal.NombreSucursal WHERE sucursal.NombreSucursal='Paginas capitalinas'")
+    listadoLibrosBogota = cursor.fetchall()
+    return listadoLibrosBogota
+
+def listadoLibrosPasto():
+    cursor.execute("SELECT libro.Titulo, sucursal.NombreSucursal FROM inventario JOIN libro ON inventario.idLibro = libro.idLibro JOIN sucursal ON inventario.NombreSucursal = sucursal.NombreSucursal WHERE sucursal.NombreSucursal='Palabras Andinas'")
+    listadoLibrosPasto = cursor.fetchall()
+    return listadoLibrosPasto
+
+def listadoLibrosCali():
+    cursor.execute("SELECT libro.Titulo, sucursal.NombreSucursal FROM inventario JOIN libro ON inventario.idLibro = libro.idLibro JOIN sucursal ON inventario.NombreSucursal = sucursal.NombreSucursal WHERE sucursal.NombreSucursal='Rincon literario cali'")
+    listadoLibrosCali = cursor.fetchall()
+    return listadoLibrosCali
+
+
+#INFORMACION DE EMPLEADOS
+def listadoEmpMedellin():
+    cursor.execute("SELECT e.NombreEmpleado,e.DocumentoEmpleado, e.FechaNacimiento, e.FechaIngreso, e.Salario, tem.TelefonosMovil, tem.TelefonosFijo FROM empleado e INNER JOIN telefonosempleado tem ON e.idTelefonosEmpleado = tem.idTelefonosEmpleado WHERE e.NombreSucursal = 'Librero del valle';")
+    infoEmpleMede = cursor.fetchall()
+    return infoEmpleMede
